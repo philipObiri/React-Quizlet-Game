f48b87a5cdedfca64ee67e61ae24adcfaa10c2a0
Philip Obiri <philipobiri3.1@gmail.com>
2023-11-01 17:24:51 +0000

    Build a resusable Next Button

---
+++ src/components/NextButton.jsx
@@ -0,0 +1,16 @@
+function NextButton({dispatch,index, numOfQuestions, answer}) {
+    if(answer === null) return null;
+    
+   if(index < numOfQuestions -1 ) return (
+        <button className="btn btn-ui" onClick={()=>(dispatch({type:"nextQuestion"}))}>
+            Next
+        </button>
+    )
+
+    if(index === numOfQuestions - 1 ) return (
+        <button className="btn btn-ui" onClick={()=>(dispatch({type:"finish"}))}>
+            Finish
+        </button>
+    )
+}
+export default NextButton;
