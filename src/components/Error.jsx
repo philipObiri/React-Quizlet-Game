0fee5cbbc787a3d677321a55073c44ee5cd54230
Philip Obiri <philipobiri3.1@gmail.com>
2023-11-01 17:26:10 +0000

    task: Build Error Component to render error message

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+function Error() {
+    return (
+      <p className="error">
+        <span>💥</span> Uh Oh! There was an error fetching the questions. :(
+      </p>
+    );
+  }
+  
+  export default Error;
+  
